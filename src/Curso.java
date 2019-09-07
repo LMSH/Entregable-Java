21776e4d3a96b272acadceb8f1c0ff8b372f1ba1
Luis <luis_soler@hotmail.com>
2019-09-07 09:21:34 -0300

    Parte B

---
+++ src/Curso.java
@@ -0,0 +1,22 @@
+public class Curso {
+    private String nombreDelCurso;
+    private Integer codigoDelCurso;
+
+    // Métodos Getter.
+    public String getNombreDelCurso() {
+        return nombreDelCurso;
+    }
+    public Integer getCodigoDelCurso() {
+        return codigoDelCurso;
+    }
+
+    // Métodos Setters.
+    public String setnombreDelCurso() {return nombreDelCurso;}
+    public Integer setcodigoDelCurso() {return codigoDelCurso;}
+
+    //Un curso es igual a otro si sus códigos de curso son iguales.
+
+
+
+
+}
