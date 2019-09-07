0bf37a2b1a8b98fffab916cdd0aad7acd51a3ea6
Luis <luis_soler@hotmail.com>
2019-09-07 08:45:12 -0300

    Parte A

---
+++ src/Alumno.java
@@ -0,0 +1,15 @@
+public class Alumno {
+    private String nombre;
+    private String apellido;
+    private Integer codigodealumno;
+
+    // Método Constructor.
+
+    public Alumno(String nombre, String apellido, Integer codigodealumno){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.codigodealumno = codigodealumno;
+    }
+
+
+}
