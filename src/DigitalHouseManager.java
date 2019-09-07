a55179ddeabf3945eb5f037a80022a8890c508ea
Luis <luis_soler@hotmail.com>
2019-09-07 11:55:02 -0300

    Parte G

---
+++ src/DigitalHouseManager.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class DigitalHouseManager {
+
+    private List<Alumno> listaDeAlumnos;
+    private List<Profesor> listaDeProfesores;
+    private List<Curso> listaDeCursos;
+    private List<Inscripcion> listaDeInscriptos;
+
+
+
+}
