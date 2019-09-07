bbace01b68b3153cc374343b55e212e062d6f957
Luis <luis_soler@hotmail.com>
2019-09-07 11:00:50 -0300

    Parte D

--- src/Curso.java
+++ src/Curso.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Curso {
     private String nombreDelCurso;
     private Integer codigoDelCurso;
+    private ProfesorTitular profesorTitular;
+    private ProfesorAdjunto profesorAdjunto;
+    private Integer cupoMaximoDeAlumnos;
+    private List<Alumno> listaDeAlumnos;
+
+
 
     // Métodos Getter.
     public String getNombreDelCurso() {
@@ -9,10 +18,30 @@ public class Curso {
     public Integer getCodigoDelCurso() {
         return codigoDelCurso;
     }
+    public ProfesorTitular getProfesorTitular() {
+        return profesorTitular;}
+    public ProfesorAdjunto getProfesorAdjunto(){
+        return profesorAdjunto;
+    }
+    public Integer getCupoMaximoDeAlumnos(){
+        return cupoMaximoDeAlumnos;
+    }
+    public List<Alumno> getListaDeAlumnos() {
+        return listaDeAlumnos;
+    }
 
     // Métodos Setters.
-    public String setnombreDelCurso() {return nombreDelCurso;}
-    public Integer setcodigoDelCurso() {return codigoDelCurso;}
+    public String setNombreDelCurso() {return nombreDelCurso;}
+    public Integer setCodigoDelCurso() {return codigoDelCurso;}
+    public ProfesorTitular setProfesorTitular(){
+        return profesorTitular;
+    }
+    public Integer setCupoMaximoDeAlumnos(){
+        return cupoMaximoDeAlumnos;
+    }
+    public ProfesorAdjunto setProfesorAdjunto(){
+        return profesorAdjunto;
+    }
 
     //Un curso es igual a otro si sus códigos de curso son iguales.
 
